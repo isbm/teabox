1eb48e2fb599521146002b9fbcf5cc4721cfd6fc
Bo Maryniuk <bo@maryniuk.net>
2022-10-28 23:16:39 +0200

    Define colors for the form buttons

--- teaboxlib/uiconf.go
+++ teaboxlib/uiconf.go
@@ -10,11 +10,17 @@ import (
 var MAIN_MENU_WIDTH int = 30
 
 // Labels
-var LABEL_BACK = "◀◃◃ Back"
-var LABEL_EXIT = "Exit ▹▹▶"
+var LABEL_BACK = "◀ Back"
+var LABEL_EXIT = "Exit ▶"
 var LABEL_SEP = "─"
+var LABEL_MORE = "…"
 
 // Colors theme
 var WORKSPACE_BACKGROUND = tcell.ColorDarkGreen
 var WORKSPACE_HEADER = tcell.ColorGreen
 var WORKSPACE_HEADER_TEXT = tcell.ColorBlack
+
+var FORM_BUTTON_BACKGROUND = tcell.ColorBlack
+var FORM_BUTTON_BACKGROUND_SELECTED = tcell.ColorLightGray
+var FORM_BUTTON_TEXT = tcell.ColorLightGray
+var FORM_BUTTON_TEXT_SELECTED = tcell.ColorBlack
