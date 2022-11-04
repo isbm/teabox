419ab03ee62dc4e6246bc2b5e4d148b17e94f3d4
Bo Maryniuk <bo@maryniuk.net>
2022-11-04 23:37:13 +0100

    Add demo of passed arguments and timed status update

--- example/modules/example-command/example-module.sh
+++ example/modules/example-command/example-module.sh
@@ -26,6 +26,18 @@ function do_something() {
     echo "Done"
 }
 
+set_status "Checking the arguments"
+echo $@
+
+max_sec=5
+left_sec="$max_sec"
+for (( i=1; i<=$max_sec; i++ ))
+do
+    set_status "Waiting $left_sec seconds..."
+    sleep 1
+    let "left_sec-=1"
+done
+
 set_status "Calling Something"
 do_something $1
 
