ef84d5fdb7618a3a2ec1829706174cff024a5948
Bo Maryniuk <bo@maryniuk.net>
2022-11-04 22:28:21 +0100

    Add socket interaction with the UI to the module script

--- example/modules/example-command/example-module.sh
+++ example/modules/example-command/example-module.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/bash
 
+SOCK="/tmp/example-callback.sock"
+
+#
+# Set status of the logging output
+#
+function set_status() {
+    m=$1
+    $(echo "LOGGER-STATUS::$m" | nc -w0 -U $SOCK)
+}
+
+#
+# Set title of the logging output widget
+#
+function set_title() {
+    m=$1
+    $(echo "LOGGER-TITLE::$m" | nc -w0 -U $SOCK)
+}
+
+
 function do_something() {
     location=$1
     echo "Putting something to $location..."
@@ -7,5 +26,10 @@ function do_something() {
     echo "Done"
 }
 
+set_status "Calling Something"
 do_something $1
+
+set_status "Calling package manager update"
 sudo apt update
+
+set_status "Finished!"
