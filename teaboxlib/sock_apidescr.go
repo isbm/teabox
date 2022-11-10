030fa3dd2141e8069d8ee197d209a5cfdc7350ae
Bo Maryniuk <bo@maryniuk.net>
2022-11-10 20:25:36 +0100

    Add API entry points.

---
+++ teaboxlib/sock_apidescr.go
@@ -0,0 +1,12 @@
+package teaboxlib
+
+// Classes for the loader window
+var INIT_SET_PROGRESS string = "init-set-progress"
+var INIT_INC_PROGRESS string = "init-inc-progress"
+var INIT_ALLOC_PROGRESS string = "init-alloc-progress"
+var INIT_SET_STATUS string = "init-set-status"
+var INIT_RESET string = "init-reset"
+
+// Logger
+var LOGGER_STATUS string = "logger-status"
+var LOGGER_TITLE string = "logger-title"
