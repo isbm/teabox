04f92f96ef20e4202a66e2b4f59a340f6b2760d3
Bo Maryniuk <bo@maryniuk.net>
2023-01-24 16:23:12 +0100

    Implement permissions conditions

--- teaboxlib/teaconditions/cond_perm.go
+++ teaboxlib/teaconditions/cond_perm.go
@@ -1,28 +1,78 @@
 package teaconditions
 
+import (
+	"fmt"
+	"os/user"
+	"strings"
+)
+
 // Condition on some file exists or is absent
 type TeaCondPerm struct {
+	currUsr *user.User
 	/* Usage:
 
-	gid:
-	 - kvm
-	 - adm
+	- gid:
+	  - kvm
+	  - adm
+	  mesage: you should be in the valid groups
 
-	uid: root
+	- uid:
+	  - root
+	  message: You are not Groot!
 
 	*/
 	BaseTeaCondition
 }
 
 // NewTeaCondPerm constructor to a file-related conditions
-func NewTeaCondPerm(clause, target string) (*TeaCondPerm, error) {
+func NewTeaCondPerm(message, clause string, targets []string) (*TeaCondPerm, error) {
 	tcf := new(TeaCondPerm)
+	tcf.message = message
+
+	switch clause {
+	case "gid", "uid":
+		tcf.clause = clause
+	default:
+		return nil, fmt.Errorf("clause should be either 'gid' or 'uid': '%s'", clause)
+	}
+
+	for _, target := range targets {
+		target = strings.TrimSpace(target)
+		if target != "" {
+			tcf.targets = append(tcf.targets, target)
+		}
+	}
+
+	if len(tcf.targets) == 0 {
+		return nil, fmt.Errorf("permission conditions require at lease one GID or UID specified")
+	}
+
+	var err error
+	tcf.currUsr, err = user.Current()
+
+	if err != nil {
+		return nil, err
+	}
 
 	return tcf, nil
 }
 
 // IsSatisfied returns if a specific condition is met for a specified user permissions
 func (tcf *TeaCondPerm) IsSatisfied() bool {
+	switch tcf.clause {
+	case "gid":
+		for _, gid := range tcf.targets {
+			if tcf.currUsr.Gid == gid {
+				return true
+			}
+		}
+	case "uid":
+		for _, uid := range tcf.targets {
+			if tcf.currUsr.Uid == uid {
+				return true
+			}
+		}
+	}
 
-	return true
+	return false
 }
