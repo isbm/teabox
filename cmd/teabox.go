d737b76c7f7d6ff1298bcfabd3d7a06cccfd56b5
Bo Maryniuk <bo@maryniuk.net>
2023-02-09 17:21:39 +0100

    Bugfix: prevents crash if a module failed to initalise

--- cmd/teabox.go
+++ cmd/teabox.go
@@ -21,12 +21,12 @@ func main() {
 	appname := path.Base(os.Args[0])
 
 	conf, err := teaboxlib.NewTeaConf(appname)
-	defer os.Remove(conf.GetSocketPath())
-
 	if err != nil {
 		fmt.Printf("Error: %s\n", err.Error())
 		os.Exit(1)
 	}
+
+	defer os.Remove(conf.GetSocketPath())
 
 	app := teabox.GetTeaboxApp().SetGlobalConfig(conf)
 	app.SetRoot(teaboxui.InitTeaboxMainWindow().GetContent(), true)
